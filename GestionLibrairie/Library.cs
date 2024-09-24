9895171dd8bb5c9f62938d4eacd2faf02e9694f2
Simon Roger <simon.roger@etud.u-picardie.fr>
2024-09-24 22:50:18 +0200

    Update .gitignore

--- GestionLibrairie/Library.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Library
-{
-    private List<Media> medias = new List<Media>();
-
-    public Media this[int numeroDeReference_IN]
-    {
-        get { return medias.FirstOrDefault(m => m.numeroDeReference.get == numeroDeReference_IN); }
-    }
-}
